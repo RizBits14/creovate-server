2aaa9cd2635485902ea8f5127f1739a501f1815d
RizBits14 <mohammad.rizwanul.islam12014@gmail.com>
2025-11-12 09:33:31 +0600

    added APIs for favourites
    the APIs for art and favourites has been added. The APIs will change later once the database is ready. This is just testing phase

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const app = express()
@@ -10,15 +10,14 @@ const port = process.env.PORT || 3000
 app.use(cors())
 app.use(express.json())
 
-const mongoose = require('mongoose')
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@riz14.17psksb.mongodb.net/?appName=Riz14`;
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 app.get('/', (req, res) => {
@@ -31,10 +30,78 @@ async function run() {
 
         const db = client.db('creovate_db')
         const artsCollections = db.collection('arts')
+        const favouriteCollection = db.collection('favourites')
 
-        app.post('/arts', async(req, res) => {
+        // Arts APIs
+        app.get('/arts', async (req, res) => {
+            const email = req.query.email
+            const query = {}
+            if (email) {
+                query.email = email
+            }
+            const cursor = artsCollections.find(query)
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.get('/arts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await artsCollections.findOne(query)
+            res.send(result)
+        })
+
+        app.post('/arts', async (req, res) => {
             const newArts = req.body
             const result = await artsCollections.insertOne(newArts)
+            res.send(result)
+        })
+
+        app.patch('/arts/:id', async (req, res) => {
+            const id = req.params.id
+            const updatedArts = req.body
+            const query = { _id: new ObjectId(id) }
+            const update = {
+                $set: {
+                    name: updatedArts.name,
+                    painting: updatedArts.painting
+                }
+            }
+            const result = await artsCollections.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.patch('/arts/:id/like', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const update = { $inc: { likes: 1 } }
+            const result = await artsCollections.updateOne(query, update)
+            res.send(result)
+        })
+
+        app.delete('/arts/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await artsCollections.deleteOne(query)
+            res.send(result)
+        })
+
+        // Favourite APIs
+        app.post('/favourites', async (req, res) => {
+            const { userEmail, artworkId } = req.body
+            const result = await favouriteCollection.insertOne({ userEmail, artworkId })
+            res.send(result)
+        })
+
+        app.get('/favourites', async (req, res) => {
+            const email = req.query.email
+            const result = await favouriteCollection.find({ userEmail: email }).toArray()
+            res.send(result)
+        })
+
+        app.delete('/favourites/:artworkkId', async (req, res) => {
+            const { artworkId } = req.params
+            const result = await favouriteCollection.deleteOne({ artworkId })
             res.send(result)
         })
 
@@ -49,5 +116,5 @@ async function run() {
 run().catch(console.dir)
 
 app.listen(port, () => {
-    console.log(`Creovate server is running on por: ${port}`)
+    console.log(`Creovate server is running on port: ${port}`)
 })
